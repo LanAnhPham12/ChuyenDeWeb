98ebc014e70661b6170ad68b3a77c7124bf0309f
HoaiBaoHong <21130281@st.hcmuaf.edu.vn>
2025-05-29 02:04:19 +0700

    install tailwind, mui, react and navigation

---
+++ e_commerce/shop-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navigations from "./customer/components/Navigations/Navigations";
+
+function App() {
+    return (
+        <div className="">
+            <Navigations />
+            <div>home</div>
+        </div>
+    );
+}
+
+export default App;
